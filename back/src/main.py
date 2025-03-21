99bee8937fd5dfcd0bcea20f38225bf3d32e140c
Bruno Gouveia Schoola <bschoola@gmail.com>
2025-03-21 18:33:42 -0300

    feat: add unicorn in python project
    feat: add unicorn in python project

--- back/src/main.py
+++ back/src/main.py
@@ -6,3 +6,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# uvicorn.run(
+#     "app:app",
+#     host=os.getenv("HOST", "0.0.0.0"),
+#     port=int(os.getenv("PORT", 8000)),
+#     # workers=int(os.getenv("WORKERS", 4)),
+#     log_level=os.getenv("LOG_LEVEL", "info"),
+# )
