4eaea06a474af82229475e119615bb81c6bf59c6
Bruno Gouveia Schoola <bschoola@gmail.com>
2025-03-26 19:05:23 -0300

    feat: add component board and workspace

--- back/src/main.py
+++ back/src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+print(f"main.py with :{app}")
 
 @app.get("/")
 async def root():
@@ -16,5 +17,5 @@ async def get_data():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, log_level="debug", # debug=True,
-                workers=1, limit_concurrency=1, limit_max_requests=1)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, log_level="debug" )#, # debug=True,
+                # workers=1, limit_concurrency=1, limit_max_requests=1)
