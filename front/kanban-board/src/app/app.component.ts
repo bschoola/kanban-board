811bdd7f40ecf1e039f5af5f07d786b35b7b2d2b
Ricardo Stebulaitis <52110022+stebulaitis@users.noreply.github.com>
2025-03-26 23:15:36 -0300

    feat: header, main and footer

--- front/kanban-board/src/app/app.component.ts
+++ front/kanban-board/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { BoardComponent } from "./board/board.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
